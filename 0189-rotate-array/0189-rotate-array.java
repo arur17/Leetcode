d2545506d50201b07c480019aa0879104830d841
Shivang Sharma <139007854+Shivang-2004@users.noreply.github.com>
2024-06-18 16:17:34 +0530

    Time: 2 ms (29.35%), Space: 56.9 MB (89.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,24 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int[] temp = new int[k];
-        if (n == 0)
-            return;
+        if(n <=1) return; // no need to rotate.
         k = k % n;
-        if (k > n)
-            return;
-        
-        for (int i = n - k; i < n; i++) {
-            temp[i - n + k] = nums[i];
-        }
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
+        if(k == 0) return;
+
+        swap(nums, 0, n-1);
+        swap(nums, 0, k-1);
+        swap(nums, k, n-1);
+
+
+    }
+
+    void swap(int[] nums, int l, int r){
+        while(l<r){
+            int t = nums[l];
+            nums[l] = nums[r];
+            nums[r] = t;
+            l++;
+            r--;
         }
     }
 }

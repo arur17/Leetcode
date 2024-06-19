7749f1325a7c16738ee6f4211f07cd9df689e233
Shivang Sharma <139007854+Shivang-2004@users.noreply.github.com>
2024-06-19 13:39:04 +0530

    Time: 492 ms (16.34%), Space: 7.6 MB (21.2%) - LeetHub

---
+++ 0136-single-number/0136-single-number.c
@@ -0,0 +1,31 @@
+int singleNumber(int* nums, int numsSize) {
+    int* hashMap = (int*)malloc(numsSize * sizeof(int) * 2); // Allocate memory for the hash map
+    int hashMapSize = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int found = 0;
+        for (int j = 0; j < hashMapSize; j += 2) {
+            if (hashMap[j] == nums[i]) {
+                hashMap[j + 1]++;
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            hashMap[hashMapSize] = nums[i];
+            hashMap[hashMapSize + 1] = 1;
+            hashMapSize += 2;
+        }
+    }
+
+    for (int i = 0; i < hashMapSize; i += 2) {
+        if (hashMap[i + 1] == 1) {
+            int singleNum = hashMap[i];
+            free(hashMap);
+            return singleNum;
+        }
+    }
+
+    free(hashMap);
+    return -1; // Error case
+}

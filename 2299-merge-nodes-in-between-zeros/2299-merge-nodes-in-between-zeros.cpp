57164ffa5569531400ed55cef811fe11a9ae2d55
Ripplecrest <139007854+ripplecrest@users.noreply.github.com>
2024-07-04 07:16:14 +0000

    Time: 410 ms (84.74%), Space: 256.7 MB (76.6%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -10,47 +10,21 @@
  */
 class Solution {
 public:
-    //return the vector from ll
-    vector<int> convertLinkedListToVector(ListNode* head) {
-        vector<int> v;
-        while (head != nullptr) {
-            v.push_back(head->val);
-            head = head->next;
-        }
-        return v;
-    }
-    // return the ll from vector
-    ListNode* convertVectorToLinkedList(vector<int>& v) {
-    if (v.empty()) {
-        return nullptr;
-    }
-    ListNode* head = new ListNode(); 
-    head->val = v[0];
-    head->next = nullptr;
-    ListNode* current = head;
-    for (size_t i = 1; i < v.size(); ++i) {
-        ListNode* newNode = new ListNode();
-        newNode->val = v[i];
-        newNode->next = nullptr;
-        current->next = newNode;
-        current = newNode;
-        }
-        return head;
-    }
-    
     ListNode* mergeNodes(ListNode* head) {
-      vector<int> inter =   convertLinkedListToVector(head);
-      vector<int> ansVt;
-      int sum = 0;
-      for(int i = 0; i<inter.size(); i++){
-        sum = sum + inter[i];
-        if(inter[i] == 0 && sum != 0){
-            ansVt.push_back(sum);
-            sum = 0;
-        }
-      }
-      ListNode *answer = convertVectorToLinkedList(ansVt);
-      return answer;
+        ListNode* modify = head->next;
+        ListNode* nextSum = modify;
 
+        while(nextSum != NULL){
+            int sum = 0;
+            while(nextSum->val != 0){
+                sum+= nextSum->val;
+                nextSum= nextSum->next;
+            }
+            modify->val = sum;
+            nextSum = nextSum->next;
+            modify->next = nextSum;
+            modify = modify->next;
+        }
+        return head->next;
     }
 };

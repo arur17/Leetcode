a34c16d00f0f2cde7607a4b19e9cdb615ec11103
Ripplecrest <139007854+ripplecrest@users.noreply.github.com>
2024-07-14 10:51:25 +0000

    Time: 3 ms (80.13%), Space: 16.8 MB (8.02%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        auto it = find(nums.begin(), nums.end(), target);
+        if(it != nums.end()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};

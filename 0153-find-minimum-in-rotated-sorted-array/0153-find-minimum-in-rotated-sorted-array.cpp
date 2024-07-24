7094c8891b0ec1848580d10467475f98937329b9
Ripplecrest <139007854+ripplecrest@users.noreply.github.com>
2024-07-24 15:41:09 +0530

    Time: 3 ms (57%), Space: 12.9 MB (15.77%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int new_laptop_yee = nums[0];
+        int n = nums.size();
+        int low = 0, high = n-1, ans = INT_MAX;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[low] <= nums[mid]){
+                ans = min(ans, nums[low]);
+                low = mid+1;
+            }
+            else{
+                ans = min(ans, nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+/*
+Extreme brute force.
+int new_laptop_yee = nums[0];
         for(int i=0; i<nums.size(); i++){
             if(nums[i]<new_laptop_yee){
                 new_laptop_yee = nums[i];
             }
         }
         return new_laptop_yee;
-    }
-};
+
+*/

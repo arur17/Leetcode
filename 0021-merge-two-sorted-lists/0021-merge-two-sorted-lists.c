44623dd7a61eb0853f8a6efcfedd9c67705dbed4
Shivang Sharma <139007854+ripplecrest@users.noreply.github.com>
2024-12-09 16:07:01 +0530

    Time: 0 ms (100%), Space: 10.8 MB (27.89%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -1,29 +1,44 @@
 
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode dummy;
-    struct ListNode* temp3 = &dummy;
-    dummy.next = NULL;
+    // struct ListNode dummy;
+    // struct ListNode* temp3 = &dummy;
+    // dummy.next = NULL;
 
-    struct ListNode* temp1 = list1;
-    struct ListNode* temp2 = list2;
+    // struct ListNode* temp1 = list1;
+    // struct ListNode* temp2 = list2;
 
-    while (temp1 != NULL && temp2 != NULL) {
-        if (temp1->val < temp2->val) {
-            temp3->next = temp1;
-            temp1 = temp1->next;
-        } else {
-            temp3->next = temp2;
-            temp2 = temp2->next;
-        }
-        temp3 = temp3->next;
+    // while (temp1 != NULL && temp2 != NULL) {
+    //     if (temp1->val < temp2->val) {
+    //         temp3->next = temp1;
+    //         temp1 = temp1->next;
+    //     } else {
+    //         temp3->next = temp2;
+    //         temp2 = temp2->next;
+    //     }
+    //     temp3 = temp3->next;
+    // }
+    // if (temp1 != NULL) {
+    //     temp3->next = temp1;
+    // }
+    // if (temp2 != NULL) {
+    //     temp3->next = temp2;
+    // }
+
+    // return dummy.next;
+
+    if(list1 == NULL){
+        return list2;
     }
-    if (temp1 != NULL) {
-        temp3->next = temp1;
-    }
-    if (temp2 != NULL) {
-        temp3->next = temp2;
+    if(list2 == NULL){
+        return list1;
     }
 
-    return dummy.next;
+    if(list1->val < list2->val){
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    } else {
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
 }
